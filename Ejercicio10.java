ce3491ee7843b835e103fa789addbf099c051aa4
Fabian Andres Chiran <111092524+xFa00@users.noreply.github.com>
2023-03-17 18:43:01 -0500

    Add files via upload

---
+++ Ejercicio10.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio10;
+import java.util.Scanner;
+
+public class Ejercicio10 {
+
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        
+        int numI,patrim,estrato;
+        double valorM;
+        String nombre;
+        
+        valorM=50000;
+                
+        System.out.println("Digite su numero de inscripcion:");
+        numI= input.nextInt();
+        
+        System.out.println("Digite su nombre:");
+        nombre= input.next();
+        
+        System.out.println("Digite su patrimonio:");
+        patrim= input.nextInt();
+        
+        System.out.println("Digite su estrato social:");
+        estrato= input.nextInt();
+        
+        if(patrim>2000000 && estrato>3){
+            valorM=valorM+(patrim*(0.03));   
+       
+       }
+    
+        System.out.println("Numero de inscripcion: "+numI);
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Pago de matricula: "+valorM);
+    
+       
+    }
+}
---
+++ Ejercicio7.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio7;
+
+import java.util.Scanner;
+
+
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        
+        double numA,numB;
+        
+        System.out.println("Digite el primer numero: ");
+        numA= input.nextDouble();
+        
+        System.out.println("Digite el segundo numero: ");
+        numB= input.nextDouble();
+        
+        if(numA>numB){
+            System.out.println(numA +" es mayor que "+ numB);
+        } else if(numA<numB){
+            System.out.println(numA +" es menor que "+ numB);
+                
+        } else{
+            System.out.println(numA +" es igual que "+ numB);
+        }
+          
+    }
+}
+
